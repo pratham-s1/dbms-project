e6a8b77fe5ad5670e780b9d47a341abfc4367ff2
Sitaraman S <sitaraman.raghav.india@gmail.com>
2024-04-03 04:02:24 +0530

    Cors fix

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:8080"],
+    origin: ["http://localhost:3000", "http://localhost:8080", "https://rrmpdbms.vercel.app"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
